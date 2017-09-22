aae141583d70542b1fb9974c6a33086589f7c908
schwim <schwim@bitrail.com>
2017-09-21 18:44:10 -0700

    revised test.py to allow for additional test functions.

--- test.py
+++ test.py
@@ -4,20 +4,22 @@ import tsxlib, time
 
 IP_ADDR = '172.16.254.254'
 mount = tsxlib.mount(IP_ADDR)
-iteration = 1
-
-while (1):
-    print ('###### Iteration #%s #######' % iteration)
-    print ('1. Talking to: %s' % mount.IP_ADDR)
-    print ('2. Version: %s  ' % mount.tsxCheck())
-    print ('3. Mount parked: %s' % mount.IsParked())
-    print ('4. Park mount: %s' % mount.ParkAndDoNotDisconnect())
-    print ('5. Mount parked: %s' % mount.IsParked())
-    print ('6. Disconnect mount: %s' % mount.Disconnect())
-    time.sleep(5)
-    iteration += 1
 
 
+def test1(count):
+    iteration = 1
+    while (iteration < count):
+        print('###### Iteration #%s #######' % iteration)
+        print('1. Talking to: %s' % mount.IP_ADDR)
+        print('2. Version: %s  ' % mount.tsxCheck())
+        print('3. Mount parked: %s' % mount.IsParked())
+        print('4. Park mount: %s' % mount.ParkAndDoNotDisconnect())
+        print('5. Mount parked: %s' % mount.IsParked())
+        print('6. Disconnect mount: %s' % mount.Disconnect())
+        time.sleep(5)
+        iteration += 1
 
 
+
+test1(5)
 
